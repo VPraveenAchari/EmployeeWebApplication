a4458d3a22a80c61b9f03d70ec75d96e3ffce25d
PraveenAchari <praveen.achari@accionlabs.com>
2023-03-28 23:57:30 +0530

    commit

--- BusinessLogic/SeviceLayer/EmployeeService.cs
+++ BusinessLogic/SeviceLayer/EmployeeService.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainLogic.DomainLayer;
+using DomainLogic.Models;
 
 namespace BusinessLogic.SeviceLayer
 {
-    public class EmployeeService
+    public class EmployeeService:IEmployeeService
     {
+        private readonly IEmployeeRepository _employeeRepository;
+        public EmployeeService(IEmployeeRepository employeeRepository) {
+            _employeeRepository = employeeRepository;
+        }
+        public int PostClient(ClientModel clientModel)
+        {
+            var clients = new ClientModel()
+            {
+                ClientId = clientModel.ClientId,
+                ClientName = clientModel.ClientName,
+                ClientAddress = clientModel.ClientAddress,
+                ClientType = clientModel.ClientType
+            };
+            var client = _employeeRepository.Database.ExecuteSqlRaw($"spPostClients {clients.ClientType},{clients.ClientName},{clients.ClientAddress}");
+            // _employee.Add(client);
+            //  _employee.SaveChanges();
+            return client;
+        }
     }
 }
--- BusinessLogic/SeviceLayer/IEmployeeService.cs
+++ BusinessLogic/SeviceLayer/IEmployeeService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainLogic.Models;
 
 namespace BusinessLogic.SeviceLayer
 {
     public interface IEmployeeService
     {
-
+        int PostClient(ClientModel clientModel);
     }
 }
